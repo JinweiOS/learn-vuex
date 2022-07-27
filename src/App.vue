7f63306b7e7ab79ea6b3f239abc857b63a795f5e
jinweiOS <jinwei.peng@outlook.com>
2022-07-27 10:30:18 +0800

    feat: vuex

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,45 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapState } from "vuex";
+import Store from '@/store/index.js'
+import HelloWorld from "./components/HelloWorld.vue";
+
+function selfMap(arr) {
+  console.log(Store)
+  const obj = {};
+  arr.forEach((stateKey) => {
+    obj[stateKey] = () => Store.state[stateKey];
+  });
+  return obj;
+
+  // {
+  //  token: () => Store.state[token]
+  //}
+}
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  mounted() {
+    console.log(this.token);
+    this.$store.commit("increment"); // 4.触发mutations(此方法只能执行同步函数)
+    this.$store.dispatch('updateToken') // 5.触发actions(此方法支持异步)
+    console.log('userInAndToke', this.$store.getters.userIdAndToken) // 3.getter的获取方法
+  },
+  computed: {
+    token: () => this.$store.state.token, // 1.获取state的值
+    // ...mapState(["token", "count", "caseId", "userId"]),
+    ...selfMap(["token", "count", "caseId", "userId"]), // 2.简写方式获取state的值
+  },
+};
 </script>
 
 <style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,36 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: 0,
+    token: '12345yu',
+    caseId: '1234567',
+    userId: 12345678
   },
-  getters: {
+  getters: { // 3.二次处理state中的数据
+    userIdAndToken(state) {
+      return state.token + state.userId;
+    } 
   },
   mutations: {
+    increment(state) {
+      state.count++
+    },
+    updateToke(state, token) {
+      state.token = token
+    }
   },
   actions: {
+    async updateToken(context) {
+      // 
+      const res = await axios.get('http://learn.api.futureruntime.com/');
+      const token = res.data.data
+      context.commit('updateToke', token)
+    }
   },
   modules: {
   }
